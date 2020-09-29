4f1a2b16ae8df5b023ad862d384c2113efee1c78
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-09-29 10:39:03 -0400

    started with multi containers for deployment

---
+++ complex/server/index.js
@@ -0,0 +1,23 @@
+const keys = require('./keys')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//postgres client setup
+const {Pool} = require('pg');
+const pgClient = new Pool ({
+    user: keys.pgUser,
+    host : keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port : keys.pgPort
+})
+pgClient.on('error',() => console.log('lost PG connection'));
+
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) =>{console.log(err)});
